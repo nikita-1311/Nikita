29ec0e15abf6d81fbcdc23696bcbbbb35bd906f1
nikita-1311 <nbhujbal609@gmail.com>
2022-09-01 15:46:36 +0530

    project1

---
+++ project1/Day-2/Day-2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Day_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 71;
+            Console.WriteLine(a+b);
+            if(a>7)
+            {
+                Console.WriteLine("a is greater than 7");
+            }
+            else
+            {
+                Console.WriteLine("7 is greater than a");
+            }
+            Console.WriteLine("please enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Value entered by you is"+name);
+        }
+    }
+}
