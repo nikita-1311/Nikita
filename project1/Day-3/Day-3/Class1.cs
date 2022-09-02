8498fbaaf73307f4b4158e7aca1012bda107a5b1
nikita-1311 <nbhujbal609@gmail.com>
2022-09-02 10:32:20 +0530

    added code for odd and even

---
+++ project1/Day-3/Day-3/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_3
+{
+    class Class1
+    {
+        public static void Main()
+        {
+            
+            Console.WriteLine("Enter number1: ");
+            int num1 =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number2: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number3: ");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+            if(num1 > num2 && num1>num3)
+            {
+                Console.WriteLine("Number 1 is Max ");
+            }
+            else if(num2 > num1 && num2 > num3)
+            {
+                Console.WriteLine("Number 2 is Max ");
+            }
+            else
+            {
+                Console.WriteLine("Number 3 is Max ");
+            }
+        }
+    }
+}
---
+++ project1/Day-3/Day-3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Day_3
+{
+    class Program
+    {
+        static void Main1(string[] args)
+        {
+            Console.WriteLine("Enter a Number: ");
+            int no = Convert.ToInt32(Console.ReadLine());
+            if(no%2==0)
+            {
+                Console.WriteLine("Number is even " + no);
+            }
+            else
+            {
+                Console.WriteLine("Number is odd " + no);
+            }
+        }
+    }
+}
