f3289f99874ebac57a1500317dc5f45b7ad9e2e7
nikita-1311 <nbhujbal609@gmail.com>
2022-09-05 10:24:35 +0530

    2d array

---
+++ project1/Day-4/Day-4/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_4
+{
+    class Class1
+    {
+        public static void Main()
+        {
+            //2d array accept from user
+            int[,] matrix = new int[2, 2];
+            for (int row = 0; row < 2; row++)
+            {
+                for (int col = 0; col < 2; col++)
+                {
+                    Console.Write("Please enter value for matrix[" + row + "," + col + "] :");
+                    matrix[row, col] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            for (int row = 0; row < 2; row++)
+            {
+                for (int col = 0; col < 2; col++)
+                {
+                    Console.Write(matrix[row, col] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ project1/Day-4/Day-4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Day_4
+{
+    class Program
+    {
+        static void Main1(string[] args)
+        {
+            //2d array
+            int[,] matrix = new int[2, 2] { { 3, 4 }, { 7, 8 } };
+            for(int row=0;row<2;row++)
+            {
+                for(int col=0;col<2;col++)
+                {
+                    Console.Write(matrix[row,col]+"\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
