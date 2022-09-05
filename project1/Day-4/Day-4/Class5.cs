ed239ba6f5ad304ba3f991205ff9fda0360c7df2
nikita-1311 <nbhujbal609@gmail.com>
2022-09-05 11:43:33 +0530

    palindrome

---
+++ project1/Day-4/Day-4/Class5.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_4
+{
+    class Class5
+    {
+        public static void Main()
+        {
+            //palindrome
+            string rev="";
+            Console.WriteLine("Please enter a value: ");
+            string nm = Console.ReadLine();
+            for (int i = nm.Length - 1; i >= 0; i--) // Reverse the string 
+            {
+                rev += nm[i].ToString();
+            }
+            if (rev == nm) // Checking whether string is palindrome or not  
+            {
+                Console.WriteLine("The given string is Palindrome \n Entered String Was {0} and reverse string is {1}", nm, rev);
+            }
+            else
+            {
+                Console.WriteLine("The given string is not Palindrome \n Entered String Was {0} and reverse string is {1}", nm, rev);
+            }
+        }
+    }
+}
