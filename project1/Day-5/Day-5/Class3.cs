ba02ff5f277c574f96fa573981265b3b8cc377a5
nikita-1311 <nbhujbal609@gmail.com>
2022-09-06 12:19:54 +0530

    Example

--- project1/Day-5/Day-5/Class3.cs
+++ project1/Day-5/Day-5/Class3.cs
@@ -5,15 +5,31 @@ using System.Text;
 namespace Day_5
 {
     //example
+    class Class3
+    {
+        public static void Main(string[] args)
+        {
+            int n = 3;
+            Person[] persons = new Person[n];
+            Console.WriteLine("Enter Name of a person: ");
+            for (int i = 0; i < n; i++)
+            {
+                persons[i] = new Person(Console.ReadLine());
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(persons[i].ToString());
+            }
+        }
+    }
     public class Person
     {
-        public string Name { get; set; }
+       public string Name { get; set; }
 
-        public Person(string name)
+       public Person(string name)
         {
             Name = name;
         }
-
         public override string ToString()
         {
             return "Hello! I am " + Name;
@@ -23,22 +39,5 @@ namespace Day_5
         {
             Name = string.Empty;
         }
-        class Class3
-        {
-            public static void Main(string[] args)
-            {
-                int n = 3;
-                Person[] persons = new Person[n];
-                Console.WriteLine("Enter Name of a person: ");
-                for (int i = 0; i < n; i++)
-                {
-                    persons[i] = new Person(Console.ReadLine());
-                }
-                for (int i = 0; i < n; i++)
-                {
-                    Console.WriteLine(persons[i].ToString());
-                }
-            }
-        }
-}
+    }
 }
