23505a5a190308c689f7e2e9ac8a9684d5f58a7e
nikita-1311 <nbhujbal609@gmail.com>
2022-09-06 10:41:34 +0530

    Example of Animal using abstract

---
+++ project1/Day-5/Day-5/Class2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_5
+{
+    abstract class Animal 
+    {
+        private string Name;
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+        public string GetName()
+        {
+            return Name;
+        }
+        public abstract void Eat();
+    }
+    class Dog:Animal
+    {
+        public override void Eat()
+        {
+            Console.WriteLine(" is Eating");
+        }
+    }
+    class Class2
+    {
+        static void Main(string[] args)
+        {
+
+            Dog dog = new Dog();
+            Console.WriteLine("Enter a dog name");
+            dog.SetName(Console.ReadLine());
+            Console.Write(dog.GetName());
+            dog.Eat();
+        }
+    }
+}
