7c2d9aa963b10966434d65b79bcd3ac9d8163a44
nikita-1311 <nbhujbal609@gmail.com>
2022-09-06 10:09:05 +0530

    jagged

---
+++ project1/Day-5/Day-5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Day_5
+{
+    class Program
+    {
+        //jagged array
+        static void Main(string[] args)
+        {
+            int[][] samplearray = new int[][]
+            {
+                new int[]{1,2},
+                new int[]{1,2,3},
+                new int[]{1,2,3,4},
+                new int[]{ 1,2,3,4,5}
+            };
+
+            int[][] jagged = new int[5][];
+            jagged[0] = new int[] { 1, 2 };
+            jagged[1] = new int[] { 1, 2, 3 };
+            jagged[2] = new int[] { 1, 2, 3, 4 };
+            jagged[3] = new int[] { 1, 2, 3, 4, 5 };
+            jagged[4] = new int[] { 1, 2, 3, 4, 5, 6 };
+
+            for (int i = 0; i < jagged.Length; i++)
+            {
+                for (int k = 0; k < jagged[i].Length; k++)
+                {
+                    Console.Write(jagged[i][k]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
