d0377d6bc18dc192de3bef85654dca0b37244640
nikita-1311 <nbhujbal609@gmail.com>
2022-09-08 09:26:36 +0530

    static keyword

---
+++ project1/Day-7/Day-7/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Day_7
+{
+    //static keyword
+    static class A
+    {
+        public static int a;
+        public static void print()
+        {
+            Console.WriteLine(a);
+        }
+    }
+    class B
+    {
+        static int a ;
+        public int b ;
+        static B()
+        {
+            a = 1;
+            Console.WriteLine("Hello I am from static class");
+        }
+        public B()
+        {
+            b = 1;
+            a = 2;
+            Console.WriteLine("Hello I am from default constructor");
+        }
+        public void print()
+        {
+            Console.WriteLine("a= " + a + "b= " + b);
+        }
+        public void show()
+        {
+            Console.WriteLine("a= " + a);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            B b = new B();
+            b.print();
+            b.show();
+        }
+    }
+}
