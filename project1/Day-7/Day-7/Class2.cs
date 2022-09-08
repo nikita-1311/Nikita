d7d52e66727ff725ff0e9e0626ddba33e14a1722
nikita-1311 <nbhujbal609@gmail.com>
2022-09-08 11:16:38 +0530

    struct using function

--- project1/Day-7/Day-7/Class2.cs
+++ project1/Day-7/Day-7/Class2.cs
@@ -9,18 +9,21 @@ namespace Day_7
     {
         public int employeeId;
         public string employeeName;
+        public int employeesal;
     };
     class Class2
     {
-        public static void Main()
+        public static void Main3()
         {
             Employee e;
-            Console.WriteLine("Please enter employee Name");
+            Console.WriteLine("Please enter employee Name: ");
             e.employeeName = Console.ReadLine();
-            Console.WriteLine("Please enter employee ID");
+            Console.WriteLine("Please enter employee ID: ");
             e.employeeId = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Empolyee salary: ");
+            e.employeesal = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Employee Details are :");
-            Console.WriteLine("EmployeeID : {0} \nEmployee Name {1} ", e.employeeId, e.employeeName);
+            Console.WriteLine("EmployeeID : {0} \nEmployee Name: {1} \nEmployee Salary: {2}", e.employeeId, e.employeeName,e.employeesal);
         }
     }
 }
---
+++ project1/Day-7/Day-7/Class3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_7
+{
+    //struct using function
+    struct EmployeeN
+    {
+        public int employeeId;
+        public string employeeName;
+        public int employeesal;
+        public void GetData()
+        {
+            Console.WriteLine("Please enter employee Name: ");
+            employeeName = Console.ReadLine();
+            Console.WriteLine("Please enter employee ID: ");
+            employeeId = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Empolyee salary: ");
+            employeesal = Convert.ToInt32(Console.ReadLine());
+        }
+        public void Print()
+        {
+            Console.WriteLine("Employee Details are :");
+            Console.WriteLine("EmployeeID : {0} \nEmployee Name: {1} \nEmployee Salary: {2}", employeeId, employeeName,employeesal);
+        }
+    };
+    class Class3
+    {
+        public static void Main()
+        {
+
+            EmployeeN e = new EmployeeN();
+            e.GetData();
+            e.Print();
+        }
+    }
+}
