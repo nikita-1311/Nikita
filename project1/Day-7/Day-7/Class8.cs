0ba444132cbbee9cb852dd6b514759191a1251e5
nikita-1311 <nbhujbal609@gmail.com>
2022-09-08 17:58:07 +0530

    Non generic collection

--- project1/Day-7/Day-7/Class8.cs
+++ project1/Day-7/Day-7/Class8.cs
@@ -7,9 +7,10 @@ namespace Day_7
 {
     class Class8
     {
-        //ArrayList
-        public static void Main()
+        //Non-generic Collection
+        public static void Main9()
         {
+            Console.WriteLine("-----ArrayList-----");
             ArrayList al = new ArrayList();
             string str = "Hello";
             int x = 10;
@@ -19,6 +20,52 @@ namespace Day_7
             al.Add(d);
             al.Remove(x);
             foreach (var item in al)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("-----HashTable-----");
+            Hashtable ht = new Hashtable();
+            ht.Add("asp", "asp.net");
+            ht.Add("cs", "cs.net");
+            ht.Add("vb", "vb.net");
+            ht.Remove("cs");   
+            foreach (DictionaryEntry item in ht)
+            {
+                Console.WriteLine(item.Key + "===>" + item.Value);
+            }
+
+            Console.WriteLine("-----SortedList-----");
+            SortedList sl = new SortedList();
+            sl.Add("ora", "oracle");
+            sl.Add("asp", "asp.net");
+            sl.Add("cs", "cs.net");
+            sl.Add("vb", "vb.net");
+
+            foreach (DictionaryEntry item in sl)
+            {
+                Console.WriteLine(item.Key + "===>" + item.Value);
+            }
+            Console.WriteLine("-----Stack-----");
+            Stack st = new Stack();
+            st.Push("oracle");
+            st.Push("asp.net");
+            st.Push("cs.net");
+            st.Push("vb.net");
+
+            foreach (var item in st)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("-----Queues-----");
+            Queue q = new Queue();
+            q.Enqueue("oracle");
+            q.Enqueue("asp.net");
+            q.Enqueue("cs.net");
+            q.Enqueue("vb.net");
+
+            foreach (var item in q)
             {
                 Console.WriteLine(item);
             }
---
+++ project1/Day-7/Day-7/Class9.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_7
+{
+    class Class9
+    {
+        public static void Main()
+        {
+            Console.WriteLine("========List==============");
+            List<int> al = new List<int>();
+            al.Add(1);
+            al.Add(2);
+            al.Add(3);
+            al.Remove(1);
+            foreach (var item in al)
+            {
+                Console.WriteLine(item);
+            }
+
+                Console.WriteLine("========Dictionary==============");
+                Dictionary<string, string> d = new Dictionary<string, string>();
+                d.Add("asp", "asp.net");
+                d.Add("cs", "cs.net");
+                d.Add("vb", "vb.net");
+                foreach (KeyValuePair<string, string> item in d)
+                {
+                    Console.WriteLine(item.Key + "===>" + item.Value);
+                }
+
+            Console.WriteLine("========SortedList==============");
+            SortedList<string, string> sl = new SortedList<string, string>();
+            sl.Add("ora", "oracle");
+            sl.Add("asp", "asp.net");
+            sl.Add("cs", "cs.net");
+            sl.Add("vb", "vb.net");
+            foreach (KeyValuePair<string, string> item in sl)
+            {
+                Console.WriteLine(item.Key + "===>" + item.Value);
+            }
+
+            Console.WriteLine("========Stack==============");
+            Stack<string> st = new Stack<string>();
+            st.Push("oracle");
+            st.Push("asp.net");
+            st.Push("cs.net");
+            st.Push("vb.net");
+
+            foreach (var item in st)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("========Queues==============");
+            Queue<string> q = new Queue<string>();
+            q.Enqueue("oracle");
+            q.Enqueue("asp.net");
+            q.Enqueue("cs.net");
+            q.Enqueue("vb.net");
+
+            foreach (var item in q)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+    } 
+
