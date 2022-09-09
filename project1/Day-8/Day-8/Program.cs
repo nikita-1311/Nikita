be29dcbc7c49c6c040ef9f3be9eabadf71b7b5da
nikita-1311 <nbhujbal609@gmail.com>
2022-09-09 10:02:09 +0530

    exception handling

---
+++ project1/Day-8/Day-8/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Day_8
+{
+    class Program
+    {
+        //exception handling
+        static void Main(string[] args)
+        {
+            
+                int a = 7, b = 0;
+                int c;
+                int[] a1 = new int[3];
+            try
+                {
+                    c= a / b;
+                a1[3] = 90;
+                }
+                catch (DivideByZeroException ex)
+                {
+                    Console.WriteLine("Some error Occured");
+                    Console.WriteLine(ex.Message);
+                }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("Some error Occured in array");
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Hi I am coming from finally block");
+            }
+
+        }
+
+    }    
+    }
+
