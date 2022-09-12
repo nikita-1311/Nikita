6ee86dff9488df9f9021b2e7ea23ee64f800c576
nikita-1311 <nbhujbal609@gmail.com>
2022-09-12 17:35:08 +0530

    Exampl 2

---
+++ project1/Day-9/Day-9/Class2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Day_9
+{
+    //json file
+    class Class2
+    {
+        class student
+        {
+            public student()
+            {
+                Roll_No= 1;
+                StudentName = "Nikita";
+            }
+            public int Roll_No { get; set; }
+            public string StudentName { get; set; }
+
+            public void WriteInFile()
+            {
+
+            }
+        }
+        public static void Main3()
+        {
+            //c# code to json format
+            student student = new student();
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(student);
+            Console.WriteLine(json);
+            var studentObj = Newtonsoft.Json.JsonConvert.DeserializeObject<student>(json);
+            //json to file
+            System.IO.FileStream fs = new FileStream("student.json", FileMode.Append, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.WriteLine(student.StudentName);
+            sw.WriteLine(student.Roll_No);
+            sw.Flush();
+            sw.Close();
+            fs.Close();
+
+            //Reading from a file
+            FileStream fsR = new FileStream("student.Json", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fsR);
+            Console.WriteLine("======Content from file========");
+            sr.BaseStream.Seek(0, SeekOrigin.Begin);
+            while (student!= null)
+            {
+                Console.WriteLine(student.StudentName);
+                student.StudentName = sr.ReadLine();
+                Console.WriteLine(student.Roll_No);
+                student.Roll_No = Convert.ToInt32(sr.ReadLine());
+                break;
+            }
+            sr.Close();
+            fsR.Close();
+        }
+    }
+}
---
+++ project1/Day-9/Day-9/Class3.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Day_9
+{
+    class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+        public int EmployeeSalary { get; set; }
+
+        public void GetData()
+        {
+            Console.WriteLine("Please enter your EmployeeID");
+            EmployeeID = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter your EmployeeName");
+            EmployeeName = Console.ReadLine();
+            Console.WriteLine("Please enter your Salary");
+            EmployeeSalary = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+    class Class3
+    {
+        public static void Main()
+        {
+            List<Employee> employeeList = new List<Employee>();
+            Employee e = new Employee();
+            e.GetData();
+            employeeList.Add(e);
+            //c# to json
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(e);
+            Console.WriteLine(json);
+            //json to c#
+            var eObj = Newtonsoft.Json.JsonConvert.DeserializeObject<Employee>(json);
+            FileStream f = new FileStream("emp.json",FileMode.Append,FileAccess.Write);
+            StreamWriter s = new StreamWriter(f);
+            s.WriteLine(e.EmployeeID);
+            s.WriteLine(e.EmployeeName);
+            s.WriteLine(e.EmployeeSalary);
+            s.Flush();
+            s.Close();
+            f.Close();
+
+            //reading json
+            FileStream fsR = new FileStream("emp.Json", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fsR);
+            Console.WriteLine("======Content from file========");
+            sr.BaseStream.Seek(0, SeekOrigin.Begin);
+            while (e!= null)
+            {
+                Console.WriteLine("Employee ID:"+e.EmployeeID);
+                e.EmployeeID = Convert.ToInt32(sr.ReadLine());
+                Console.WriteLine("Employee Name: " +e.EmployeeName);
+                e.EmployeeName = sr.ReadLine();
+                Console.WriteLine("Employee Salary: "+e.EmployeeSalary);
+                e.EmployeeSalary = Convert.ToInt32(sr.ReadLine());
+                break;
+            }
+            sr.Close();
+            fsR.Close();
+        }
+    }
+}
