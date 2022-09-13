606927558241595206a5e35fee56e2a8ae519384
nikita-1311 <nbhujbal609@gmail.com>
2022-09-13 09:39:09 +0530

    Multicast Delegate

---
+++ project1/Day-10/Day-10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Day_10
+{
+    //Multicast Delegate
+    class Program
+    {
+        public delegate void Rectangle(double l, double b);
+        public void area(double l, double b)
+        {
+            Console.WriteLine("Area od a rectangle is: " + l * b);
+        }
+        public void perimeter(double l, double b)
+        {
+            Console.WriteLine("Perimeter of a rectangle is: " + 2 * (l + b));
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Rectangle r = new Rectangle(p.area);
+            //calling second method
+            r = r + p.perimeter;
+            //invoke function
+            r.Invoke(10, 20);
+        }
+
+
+    }
+}
