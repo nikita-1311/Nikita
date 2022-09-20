bdae32e05a66cb1a4a331712784d1e1559243a41
nikita-1311 <nbhujbal609@gmail.com>
2022-09-20 10:23:06 +0530

    Connection with DB

---
+++ project1/ConnectWithDB/ConnectWithDB/Class1.cs
@@ -0,0 +1,49 @@
+﻿using ConnectWithDB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConnectWithDB
+{
+    class Class1
+    {
+        //update, delete operations
+        public static void printData(List<Test> data)
+        {
+            Console.WriteLine("ID | TEXT");
+            //iteration on db data
+            foreach (var item in data)
+            {
+                Console.WriteLine(item.Id + "   " + item.Text);
+            }
+        }
+        public static void Main()
+        {
+            TestDBContext db = new TestDBContext();
+            var data = db.Tests;
+            printData(data.ToList());
+            Console.WriteLine("Please select any id to update the data");
+            int id = Convert.ToInt32(Console.ReadLine());
+
+            var UpdateData = db.Tests.Where(x => x.Id == id).FirstOrDefault();
+            Console.WriteLine("Please enter some text that you want to update");
+            var input = Console.ReadLine();
+            UpdateData.Text = input;
+
+            db.Tests.Update(UpdateData);
+            db.SaveChanges();
+            Console.WriteLine("database record is updated succesully");
+            printData(data.ToList());
+
+            Console.WriteLine("Please select any id to delete the data");
+            int id2 = Convert.ToInt32(Console.ReadLine());
+            var deleteItem = db.Tests.Where(x => x.Id == id2).FirstOrDefault();
+            db.Tests.Remove(deleteItem);
+            db.SaveChanges();
+            Console.WriteLine("database record is deleted succesully");
+            printData(data.ToList());
+
+        }
+    }
+}
--- project1/ConnectWithDB/ConnectWithDB/Program.cs
+++ project1/ConnectWithDB/ConnectWithDB/Program.cs
@@ -5,15 +5,22 @@ namespace ConnectWithDB
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main1(string[] args)
         {
             TestDBContext db = new TestDBContext();
-            var data = db.Tests;
-            Console.WriteLine("Id | Text");
-            foreach(var item in data)
-            {
-                Console.WriteLine(item.Id + "   " + item.Text);
-            }
+            //var data = db.Tests;
+            //Console.WriteLine("Id | Text");
+            //foreach(var item in data)
+            //{
+            //   Console.WriteLine(item.Id + "   " + item.Text);
+            // }
+
+            Test obj = new Test();
+            obj.Text = "Shruti";
+
+            db.Tests.Add(obj);
+            db.SaveChanges();
+            Console.WriteLine("Record is added");
         }
     }
 }
